eb4f5a15acb2d9919768bfd85c805bbcc58f40e1
John Doe <6mxo89sqh@relay.firefox.com>
2021-06-03 19:27:12 +0530

    fixes #448 need someone to verify.

--- src/utils.py
+++ src/utils.py
@@ -9,8 +9,8 @@ from ratelimit import handle_rate_limited
 
 BOOKING_URL = "https://cdn-api.co-vin.in/api/v2/appointment/schedule"
 BENEFICIARIES_URL = "https://cdn-api.co-vin.in/api/v2/appointment/beneficiaries"
-CALENDAR_URL_DISTRICT = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={0}&date={1}"
-CALENDAR_URL_PINCODE = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode={0}&date={1}"
+CALENDAR_URL_DISTRICT = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={0}&date={1}"
+CALENDAR_URL_PINCODE = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={0}&date={1}"
 CAPTCHA_URL = "https://cdn-api.co-vin.in/api/v2/auth/getRecaptcha"
 OTP_PUBLIC_URL = "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP"
 OTP_PRO_URL = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
@@ -349,6 +349,8 @@ def check_calendar_by_district(
         4. Returns list of vaccination centers & slots if available
     """
     try:
+        request_header = copy.deepcopy(request_header)
+        del request_header["Authorization"]
         print(
             "==================================================================================="
         )
